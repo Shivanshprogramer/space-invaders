8531061ddd1ebed92abbf0ef9c019509707e4ebc
Tina <tinapatil27121998@gmail.com>
2019-07-18 16:16:44 +0530

    Add player bullets and enemy bullets

--- space_invaders_group.py
+++ space_invaders_group.py
@@ -1,4 +1,5 @@
 from pygame import *
+import random
 
 init()
 
@@ -10,6 +11,7 @@ display.set_caption("Space Invader")
 
 invaderImage = image.load('alien.jpg')
 lives_image= image.load('heart.jpg')
+playerImage= image.load('ship.png')
 
 #colors palette
 BLACK = ( 0, 0, 0)
@@ -80,7 +82,7 @@ def drawInvaders(invaderList, screen):
     global player
     for i in invaderList:
         screen.blit(invaderImage, i)
-    playerImage= image.load("ship.png")
+    
     screen.blit(playerImage, player)
 gameOver = False
 
@@ -88,7 +90,20 @@ def play():
     global gameOver, i_move_y, i_move_x, invaderList, screen, paused_state,lives, player
     # set up invaders
     invaderList = setUpInvaders()
+    # set up player
+    px = 0
     player = Rect(600, 600, 100, 100)
+
+    dx = 20
+    frameCount = 0
+    #set up bullets 
+    playerBullets = []
+
+    # set up enemy bullets
+
+    enemyBullets = []
+    probabilityToFire = 0.0001 # this will control how often the enemy will fire
+    maxEnemyBullets = 8 # stop there being too many bullets on screen at once
 
     #initialising the values when game starts
     i_move_x = 1
@@ -104,7 +119,63 @@ def play():
                 initial()
             elif e.type == KEYDOWN and e.key == K_p:
                 pause()
+            elif e.type == KEYDOWN:
+                if e.key == K_ESCAPE:
+                    initial() 
+                if e.key == K_LEFT:
+                    px = -3
+                if e.key == K_RIGHT:
+                    px = 3
+                if len(playerBullets) < 2:
+                        playerBullets.append(Rect(player.x+50, player.y,5,20))
+            elif e.type == KEYUP:
+                if e.key == K_LEFT or e.key == K_RIGHT:
+                    px = 0
 
+        if frameCount >= 100:
+            #dx = moveAliens(invaderList, dx)
+            frameCount = 0
+        frameCount = frameCount + 1
+        # move Player
+        if player.left+px < 0 or player.right+px > width : 
+            px = 0
+            player.move_ip(px, 0)
+
+        #move bullets and check collision
+        
+        for b in playerBullets:
+            b.move_ip(0,-3)
+            if b.y < 0 :
+                playerBullets.remove(b)
+                for i in invaderList :
+                    if i.colliderect(b):
+                        invaderList.remove(i)
+                        playerBullets.remove(b)
+
+       # control enemy bullets fire
+        for i in invaderList:
+            fireChance = random.random() # pick a number from 0 to 1
+            if fireChance <= probabilityToFire  and len(enemyBullets) <= maxEnemyBullets:
+                enemyBullets.append(Rect(i.x,i.y,5,20))
+
+        # move enemy bullets and check collision with player
+        for b in enemyBullets:
+            b.move_ip(0,3)
+            if b.colliderect(player):
+                lives=lives-1
+                enemyBullets.remove(b)
+
+            if b.y > height :
+                enemyBullets.remove(b)
+
+        # drawPlayer
+        screen.blit(playerImage, player)
+
+
+
+        
+        
+                
         pressed= key.get_pressed()
 
         if pressed[K_RIGHT]==1:
@@ -117,7 +188,6 @@ def play():
         myfont = font.SysFont('Comic Sans MS', 18)
         textsurface = myfont.render('ESC - quit the game       P - pause the game', False, BLUE)
         screen.blit(textsurface, (30, 10))
-
         count=0
         while count<lives:
             screen.blit(lives_image,((width-150)+(count*30),10))
@@ -136,6 +206,12 @@ def play():
 
         #draw invaders
         drawInvaders(invaderList, screen)
+
+        # draw bullets
+        for b in playerBullets:
+            draw.rect(screen, (255, 255, 255), b)
+            for b in enemyBullets:
+                draw.rect(screen, (255, 0, 0), b)
 
         if paused_state==1:
             myfont = font.SysFont('Comic Sans MS', 60)
